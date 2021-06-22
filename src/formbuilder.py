f8e51ccffe3d7a6d82a66381122db4e6e4f95e4b
Alexey Pyatov <alexey.pyatov@sas.com>
2021-06-22 23:50:43 +0300

    readme fix + formbuilder.fix

--- src/formbuilder.py
+++ src/formbuilder.py
@@ -19,24 +19,25 @@ class FormBuilder(wx.Frame):
         panel.SetScrollbars(0, 1, 0, 0)
 
         sizer = wx.GridBagSizer(15,15)
+        i = 0
 
         icon = wx.StaticBitmap(panel, bitmap=wx.Bitmap(logo))
-        sizer.Add(icon, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.ALIGN_LEFT, border=5)
-
+        sizer.Add(icon, pos=(i, 0), flag=wx.TOP|wx.LEFT|wx.ALIGN_LEFT, border=5)
         text1 = wx.StaticText(panel, label=title)
-        sizer.Add(text1, pos=(0, 2), flag=wx.TOP|wx.RIGHT, border=15)
+        sizer.Add(text1, pos=(i, 2), flag=wx.TOP|wx.RIGHT, border=15)
+        i = i + 1
 
         line = wx.StaticLine(panel)
         sizer.Add(line, pos=(1, 0), span=(1, 5),
         flag=wx.EXPAND|wx.BOTTOM, border=10)
+        i = i + 1
 
-        i = 2
         for field in fields.items():
             text = wx.StaticText(panel, label=field[1]['label'])
             sizer.Add(text, pos=(i, 0), flag=wx.LEFT, border=10)
             tc = wx.TextCtrl(panel, value=str(field[1]['value']), name=field[0])
             sizer.Add(tc, pos=(i, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND)
-            i = i+1
+            i = i + 1
 
         button = wx.Button(panel, label="Заполнить договор", size=(500,30))
         sizer.Add(button, pos=(i, 1), span=(1, 2), flag=wx.BOTTOM|wx.RIGHT, border=10)
