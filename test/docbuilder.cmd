1a5876626dd4efa2a1f6a92f0ebda0a8862011cc
Alexey Pyatov <alexey.pyatov@sas.com>
2021-06-23 13:11:55 +0300

    small fix

--- test/docbuilder.cmd
+++ test/docbuilder.cmd
@@ -1,1 +1,1 @@
-python src\docbuilder.py -t "templates\KZVG_Contract_Template.docx" -d "data\210620_Contract_KZVG_Roga.yaml" -o "out\KZVG_Contract_filled.docx"
+python src\docbuilder.py -t "templates\KZVG_Contract_Template.docx" -d "data\210620_Contract_Roga.yaml" -o "out\KZVG_Contract_filled.docx"
