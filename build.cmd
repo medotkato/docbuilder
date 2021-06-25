dcd746d262d3d827cb2eb362c93378e39bd1f3e2
Alexey Pyatov <alexey.pyatov@sas.com>
2021-06-25 18:29:03 +0300

    v.0.66 - build.cmd fix

--- build.cmd
+++ build.cmd
@@ -25,3 +25,5 @@ robocopy .\ %projdir% *.md /FFT /R:3 /W:10 /Z /NP /NDL > nul
 robocopy .\ %projdir% *.yaml /FFT /R:3 /W:10 /Z /NP /NDL > nul
 
 echo "bin\formbuilder.exe -c config.yaml" > %projdir%\Zapolnyator_3000.cmd
+
+echo "current build is here: %projdir%"
